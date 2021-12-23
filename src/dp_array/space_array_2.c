76db020279ce6cb9680bce9588154ded0f882457
Valaaz <val.zancoth@gmail.com>
2021-12-23 13:27:41 +0100

    correct space_array_2.c

--- src/dp_array/space_array_2.c
+++ src/dp_array/space_array_2.c
@@ -13,13 +13,13 @@ void init_opt_chm(struct states_array_t *states)
         for (int bag = 0; bag <= states->Vmax; bag += 1)
         {
             int idx = obj * bag; /* A VERIFIER */
-            states->OPT[idx] = 0;
-            states->CHM[idx] = 0;
+            states->OPT[idx] = UNDTR;
+            states->CHM[idx] = UNDTR;
         }
     }
 
     for (int bag = 0; bag <= states->Vmax; bag += 1)
     {
-        states->CHM[bag] = 0;
+        states->CHM[bag] = UNDTR;
     }
 }
